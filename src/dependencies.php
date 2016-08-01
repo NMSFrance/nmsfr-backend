3489016fa43b16df8c89eac0ec712b2a6b959d08
kevin.gay@epitech.eu <kevin.gay@epitech.eu>
2016-08-01 21:47:10 +0200

    Configuration File DB + Path

--- src/dependencies.php
+++ src/dependencies.php
@@ -1,20 +1,30 @@
 <?php
-  // DIC configuration
+// DIC configuration
+$container = $app->getContainer();
 
-  $container = $app->getContainer();
+// view renderer
+$container['renderer'] = function ($c) {
+  $settings = $c->get('settings')['renderer'];
+  return new Slim\Views\PhpRenderer($settings['template_path']);
+};
 
-  // view renderer
-  $container['renderer'] = function ($c) {
-    $settings = $c->get('settings')['renderer'];
-    return new Slim\Views\PhpRenderer($settings['template_path']);
-  };
+// monolog
+$container['logger'] = function ($c) {
+  $settings = $c->get('settings')['logger'];
+  $logger = new Monolog\Logger($settings['name']);
+  $logger->pushProcessor(new Monolog\Processor\UidProcessor());
+  $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], Monolog\Logger::DEBUG));
+  return $logger;
+};
 
-  // monolog
-  $container['logger'] = function ($c) {
-    $settings = $c->get('settings')['logger'];
-    $logger = new Monolog\Logger($settings['name']);
-    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], Monolog\Logger::DEBUG));
-    return $logger;
-  };
+// Service factory for the ORM
+$container['db'] = function ($container) {
+  $capsule = new \Illuminate\Database\Capsule\Manager;
+  $capsule->addConnection($container['settings']['db']);
+
+  $capsule->setAsGlobal();
+  $capsule->bootEloquent();
+
+  return $capsule;
+};
 ?>
